2bf1b9a5d1f7b96c0b14fd238f80d3174b191950
Ziggy <davidarogers@protonmail.com>
2022-02-02 15:53:33 -0800

    error handling on SDL

--- Makefile
+++ Makefile
@@ -1,8 +1,9 @@
 ##
 # CHIPPIE
 # @version 0.1
-CFLAGS=-Wall -ggdb -std=c11 -pedantic
-LIBS=
+PKGS=sdl2
+CFLAGS=-Wall -ggdb -std=c11 -pedantic `pkg-config --cflags sdl2`
+LIBS=`pkg-config --libs sdl2`
 
 chippie: main.c
 	$(CC) $(CFLAGS) -o chippie main.c $(LIBS)
--- main.c
+++ main.c
@@ -3,29 +3,44 @@
 #include <SDL2/SDL.h>
 #include <stdbool.h>
 
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 320
+
 int main(int argc, char** argv) {
-    SDL_Init(SDL_INIT_EVERYTHING);
+
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+        printf("SDL error: %s\n", SDL_GetError());
+        exit(1);
+    } else {
+        printf("SDL initialized correctly.\n");
+    }
 
     SDL_Window* window = SDL_CreateWindow(
         "Chippie Window",
         SDL_WINDOWPOS_UNDEFINED,
         SDL_WINDOWPOS_UNDEFINED,
-        640, 320, SDL_WINDOW_SHOWN
+        WINDOW_WIDTH, WINDOW_HEIGHT,
+        SDL_WINDOW_SHOWN
     );
+
+    if (window == NULL) {
+        printf("SDL error: %s\n", SDL_GetError());
+        exit(1);
+    }
 
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_TEXTUREACCESS_TARGET);
 
     while(true) {
-        SDL_SetRenderDrawColor(renderer, 0, 0, 170, 0); // classic C64 bg.
-        SDL_RenderClear(renderer); // clears with bg color.
-        SDL_SetRenderDrawColor(renderer, 187,187,187,0); // set draw color gray.
+        SDL_SetRenderDrawColor(renderer, 0, 0, 170, 0); // classic C64 bg
+        SDL_RenderClear(renderer); // clears with bg color
+        SDL_SetRenderDrawColor(renderer, 187,187,187,0); // set draw color gray
         SDL_Rect r;
         r.x = 0;
         r.y = 0;
         r.w = 40;
         r.h = 40;
-        SDL_RenderDrawRect(renderer, &r); // draws rect to screen, need to present.
-        SDL_RenderPresent(renderer); // present changes.
+        SDL_RenderDrawRect(renderer, &r); // draws rect to screen, need to present
+        SDL_RenderPresent(renderer); // present changes
     }
 
     SDL_DestroyWindow(window);
