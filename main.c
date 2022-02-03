eda9000ad4efea80f94f06b2739cd28fc9777e95
Ziggy <davidarogers@protonmail.com>
2022-02-02 16:03:16 -0800

    sdl error handling better

--- main.c
+++ main.c
@@ -1,36 +1,52 @@
 #include <stdio.h>
 #include <math.h>
 #include <SDL2/SDL.h>
-#include <stdbool.h>
 
 #define WINDOW_WIDTH 640
 #define WINDOW_HEIGHT 320
 
-int main(int argc, char** argv) {
-
-    if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
-        printf("SDL error: %s\n", SDL_GetError());
+int sdl_check_error(int code) {
+    if (code < 0) {
+        fprintf(stderr, "SDL error: %s\n", SDL_GetError());
         exit(1);
-    } else {
-        printf("SDL initialized correctly.\n");
     }
+    return code;
+}
 
-    SDL_Window* window = SDL_CreateWindow(
+void *sdl_check_pointer(void *ptr) {
+    if (ptr == NULL) {
+        fprintf(stderr, "SDL error: %s\n", SDL_GetError());
+        exit(1);
+    }
+    return ptr;
+}
+
+
+int main(int argc, char **argv) {
+    sdl_check_error(SDL_Init(SDL_INIT_EVERYTHING));
+
+    SDL_Window *window = SDL_CreateWindow(
         "Chippie Window",
         SDL_WINDOWPOS_UNDEFINED,
         SDL_WINDOWPOS_UNDEFINED,
         WINDOW_WIDTH, WINDOW_HEIGHT,
         SDL_WINDOW_SHOWN
     );
+    sdl_check_pointer(window);
 
-    if (window == NULL) {
-        printf("SDL error: %s\n", SDL_GetError());
-        exit(1);
-    }
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_TEXTUREACCESS_TARGET);
+    sdl_check_pointer(renderer);
 
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_TEXTUREACCESS_TARGET);
-
-    while(true) {
+    int quit = 0;
+    while(!quit) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT: {
+                    quit = 1;
+                } break;
+            }
+        }
         SDL_SetRenderDrawColor(renderer, 0, 0, 170, 0); // classic C64 bg
         SDL_RenderClear(renderer); // clears with bg color
         SDL_SetRenderDrawColor(renderer, 187,187,187,0); // set draw color gray
