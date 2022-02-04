5b43c92f239bb704629c8cea1cc39d68d50f0083
Ziggy <davidarogers@protonmail.com>
2022-02-03 20:42:34 -0800

    memory implementations

--- include/chip8memory.c
+++ include/chip8memory.c
@@ -1,5 +1,9 @@
 #include "chip8memory.h"
 
-int memory_set(int addr, unsigned char val, Chip8memory *memory) {}
+void memory_set(int addr, unsigned char val, Chip8memory *memory) {
+    memory->memory[addr] = val;
+}
 
-unsigned char memory_get(int addr, Chip8memory *memory) {}
+unsigned char memory_get(int addr, Chip8memory *memory) {
+    return memory->memory[addr];
+}
--- include/chip8memory.h
+++ include/chip8memory.h
@@ -6,6 +6,6 @@ typedef struct {
     unsigned char memory[CHIP8_MEMORY_SIZE];
 } Chip8memory;
 
-int memory_set(int addr, unsigned char val, Chip8memory *memory);
+void memory_set(int addr, unsigned char val, Chip8memory *memory);
 unsigned char memory_get(int addr, Chip8memory *memory);
 #endif // CHIP8MEMORY_H_
